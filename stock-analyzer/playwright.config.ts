a0e041dbb4269f93e96e785df351bb73a6123431
seungjae3908-source <seungjae3908@gmail.com>
2026-08-11 12:26:54 +0900

    test(browser): scope remote-font disable to local CI

--- stock-analyzer/playwright.config.ts
+++ stock-analyzer/playwright.config.ts
@@ -32,7 +32,7 @@ export default defineConfig({
   use: {
     baseURL,
     browserName: 'chromium',
-    launchOptions: { args: ['--disable-remote-fonts'] },
+    launchOptions: stagingMode ? undefined : { args: ['--disable-remote-fonts'] },
     hasTouch: true,
     actionTimeout: 20_000,
     navigationTimeout: 30_000,
