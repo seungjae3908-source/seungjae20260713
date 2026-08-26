68ea7c31128423a313235357eb67457b7efa93d2
seungjae3908-source <seungjae3908@gmail.com>
2026-08-27 01:01:32 +0900

    test(scanner): register directional runtime regression

--- api-server/src/services/crypto-signal-scanner.service.test.ts
+++ api-server/src/services/crypto-signal-scanner.service.test.ts
@@ -1,5 +1,6 @@
 import test from 'node:test';
 import assert from 'node:assert/strict';
+import './crypto-futures-directional-scanner.service.test';
 import {
   createCryptoSignalScannerService,
   type CryptoCandle,
